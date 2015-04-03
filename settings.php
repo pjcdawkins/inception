3e45b5326aa234fc698ff93ba7392a8d8f864c65
Patrick Dawkins <pjcdawkins@gmail.com>
2015-04-03 16:28:28 +0100

    Admin theme

--- settings.php
+++ settings.php
@@ -9,6 +9,9 @@ if (isset($_ENV['PLATFORM_APP_DIR'])) {
   $conf['composer_manager_vendor_dir'] = $_ENV['PLATFORM_APP_DIR'] . '/composer/vendor';
 }
 
+$conf['admin_theme'] = 'shiny';
+$conf['node_admin_theme'] = TRUE;
+
 if (file_exists(__DIR__ . '/settings.local.php')) {
   include __DIR__ . '/settings.local.php';
 }
