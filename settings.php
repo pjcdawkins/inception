6f55371bf71a0dba2815eb3422fbe57dc21e09ea
Patrick Dawkins <pjcdawkins@gmail.com>
2015-04-05 11:13:05 +0100

    Move vars from settings.php to feature

--- settings.php
+++ settings.php
@@ -1,18 +1,11 @@
 <?php
 
-$conf['site_name'] = 'Platform.sh Shop';
-
-$conf['hash_salt'] = 'Y78u7MLfwZZwRvDKsLkuykIHElcCAOzn4xDH2lfxE6gpyM2uNVyAjign7qCTzWidUrKiw8tZ9bpqFiHYJurH';
-
 if (isset($_ENV['PLATFORM_APP_DIR'])) {
   $conf['composer_manager_file_dir'] = $_ENV['PLATFORM_APP_DIR'] . '/composer';
   $conf['composer_manager_vendor_dir'] = $_ENV['PLATFORM_APP_DIR'] . '/composer/vendor';
   $conf['composer_manager_autobuild_file'] = 0;
   $conf['composer_manager_autobuild_packages'] = 0;
 }
-
-$conf['admin_theme'] = 'shiny';
-$conf['node_admin_theme'] = TRUE;
 
 if (file_exists(__DIR__ . '/settings.local.php')) {
   include __DIR__ . '/settings.local.php';
